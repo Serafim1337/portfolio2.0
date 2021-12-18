a18da4e597c69db997c2516e3d8727c60832f920
Serafim <7620226@gmail.com>
2021-12-18 23:03:09 +0300

    feat: add function closing menu on link click

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function menuFunction() {
   document.querySelector('.adaptive-menu').classList.toggle('active');
   document.querySelector('body').classList.toggle('lock');
   let element = document.querySelector('#title-id');
-
   if(element.classList.contains('hidden')) {
     titleHideFunction();
   }
@@ -14,4 +13,9 @@ function menuFunction() {
 function titleHideFunction() {
   document.querySelector('.section-title').classList.toggle('hidden');
   document.querySelector('.skills-section-content').classList.toggle('menu-view-adaption');
+}
+
+function menuLinksFunction() {
+  let element = document.querySelector('#menu-id');
+  element.classList.toggle('opened');
 }
